adb13418f8e95130339b919e8dac5a524b3ca1d3
MrZeroCode <sittichot.phikhasorn@icloud.com>
2023-12-19 02:48:58 +0100

    Refactor component headings and add spacing

--- src/app/components/HomeAboutMe.tsx
+++ src/app/components/HomeAboutMe.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function HomeAboutMe(){
     return(
-        <div>
-            <h2>About me</h2>
+        <div className="mb-10">
+            <h2 className="mb-6">About me</h2>
             <p>In the kitchen or at the keyboard, I find <span className="bodoni-moda-italic-uppercase">my joy</span>. Crafting delightful websites and experimenting with new recipes are <span className="bodoni-moda-italic-uppercase">my passions</span>.</p>
             <img src="" alt="animation_img" />
             <p>Now, here&apos;s a glimpse of my <span className="bodoni-moda-italic-uppercase">tech stack</span><br/>
--- src/app/components/HomeContact.tsx
+++ src/app/components/HomeContact.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function HomeContact() {
     return (
-        <div>
-            <h2>Contact</h2>
+        <div className='my-10'>
+            <h2 className='mb-6'>Contact</h2>
             <p>Hungry for collaboration? Reach out to me via email at Sittichot@gmail.com. Let&lsquo;s cook up something extraordinary <span className="bodoni-moda-italic-uppercase">together!</span></p>
         </div>
     );
