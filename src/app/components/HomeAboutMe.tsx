cb9cb44277e4c535604a631662e5f2a319e2250d
MrZeroCode <sittichot.phikhasorn@icloud.com>
2023-12-20 14:33:48 +0100

    Fix package.json dependencies and add HomeHobbyImgAnimation component

--- src/app/components/HomeAboutMe.tsx
+++ src/app/components/HomeAboutMe.tsx
@@ -1,11 +1,13 @@
+'use client'
 import React from "react";
+import HomeHobbyImgAnimation from "./HomeHobbyImgAnimation";
 
 export default function HomeAboutMe(){
     return(
         <div className="mb-10">
             <h2 className="mb-6">About me</h2>
             <p>In the kitchen or at the keyboard, I find <span className="bodoni-moda-italic-uppercase">my joy</span>. Crafting delightful websites and experimenting with new recipes are <span className="bodoni-moda-italic-uppercase">my passions</span>.</p>
-            <img src="" alt="animation_img" />
+            <HomeHobbyImgAnimation />
             <p>Now, here&apos;s a glimpse of my <span className="bodoni-moda-italic-uppercase">tech stack</span><br/>
                 served up like a <span className="bodoni-moda-italic-uppercase">tempting burger</span></p>
             <img src="" alt="bruger" />
--- src/app/components/HomeHobbyImgAnimation.tsx
+++ src/app/components/HomeHobbyImgAnimation.tsx
@@ -1,2 +1,34 @@
-import React from 'react';
+import React from "react";
+import programmingSvg from '../../../public/programming.svg';
+import cookingSvg from '../../../public/programming.svg';
+import Image from "next/image";
+import { ReactCompareSlider, ReactCompareSliderImage, ReactCompareSliderHandle } from 'react-compare-slider';
 
+export default function HomeHobbyImgAnimation() {
+    return(
+        <div className="my-12" >
+            <ReactCompareSlider 
+            style={{
+                display: 'flex', 
+                border: '',}}
+                handle={<ReactCompareSliderHandle 
+                    style={{
+                        color: 'black',
+                        boxShadow: '',
+                    }}
+                    linesStyle={{
+                        boxShadow: '',
+                        width: '4px',
+                      }}
+                    buttonStyle={{
+                        display: 'none',
+                    }}
+                    />}
+                
+            itemOne={<ReactCompareSliderImage src="/programming.svg" alt="Image one" style={{maxHeight: '590px', width: 'auto', marginLeft: 'auto', marginRight: 'auto', backgroundColor: '#EAE2D7'}} />}
+            itemTwo={<ReactCompareSliderImage src="/cooking.svg" alt="Image two" style={{maxHeight: '590px', width: 'auto', marginLeft: 'auto', marginRight: 'auto', backgroundColor: '#EAE2D7'}} />}
+            />
+        </div>
+        
+    )
+};
